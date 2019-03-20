6bdb1d28d3e22ad4cbf97cb8719b189f73635f7f
D S <dantevs@icloud.com>
2019-03-20 22:24:32 +0100

    Create components and basic setup

---
+++ components/Accessibility.js
@@ -0,0 +1,9 @@
+import { Image } from 'mdx-deck'
+
+export default function Accessibility() {
+  return (
+    <div>
+      <h3>Accessibility run around</h3>
+    </div>
+  )
+}
---
+++ components/Bugs.js
@@ -0,0 +1,39 @@
+import { Appear, Image } from 'mdx-deck'
+
+export default function Bugs () {
+  <Appear>
+    <h3>Webiste Bug</h3>
+    <ul>
+      <Appear>
+        <li>
+          <Image src="./assets/font-issue-ticket.png" size="250px 200px" />
+        </li>
+        <li>
+          <Image src="./assets/iban-validation-ticket.png" size="250px 200px" />
+        </li>
+        <li>
+          <Image src="./assets/tool-tip-ticket.png" size="250px 200px" />
+        </li>
+        <li>
+          <Image src="./assets/accesibiity-ticket.png" size="250px 200px" />
+        </li>
+      </Appear>
+    </ul>
+    
+    
+    <ul>
+      <h3>Native App (android)</h3>
+      <Appear>
+        <li>
+          <Image src="./assets/last-android-release.png" size="250px 200px" />
+        </li>
+        <li>
+          <Image src="./assets/iban-validation-ticket.png" size="250px 200px" />
+        </li>
+        <li>
+          <Image src="./assets/accesibiity-ticket.png" size="250px 200px" />
+        </li>
+      </Appear>
+    </ul>
+  </Appear>
+}
