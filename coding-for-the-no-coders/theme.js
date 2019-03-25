cf0bf4afcb89ce2eaf23fba51d8ae236b5be04d7
D S <dantevs@icloud.com>
2019-03-25 23:44:48 +0100

    Finish deck for coding-for-no-coders.

---
+++ coding-for-the-no-coders/theme.js
@@ -0,0 +1,13 @@
+import theme from 'mdx-deck/themes'
+
+export default {
+  ...theme,
+  font: 'Open Sans, regular',
+
+  colors: {
+    text: '#FDF1AA',
+    background: '#1C1E23',
+    link: '#008F84',
+    code: '#45C3EC'
+  }
+}
